544042dca5ff385f2e864b9e715a6f82650acf52
pengfusheng <gneisu@163.com>
2016-02-26 19:20:26 +0800

    add a line

--- app/src/main/java/com/poya/pengfusheng/myapplication/MainActivity.java
+++ app/src/main/java/com/poya/pengfusheng/myapplication/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         int a = 1;
+        int b = 2;
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
